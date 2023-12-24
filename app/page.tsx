be39aaddac59ffb57bde1362db24a7e4e229e235
AlexArchibeque <wonkman33@gmail.com>
2023-12-24 14:06:07 -0800

    :sparkles: This is the next commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,12 @@ const Home = () => {
   return (
     <main>
       <div>This is the top</div>
-
       <div>This is the image fixed</div>
-
       <div>
         This is the body
         <TestComponent />
       </div>
+      Testing commit number 2
     </main>
   );
 };
