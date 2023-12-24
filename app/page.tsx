e23ccea7e3ce8ebdd1eaf8d598a005b9c6593380
AlexArchibeque <wonkman33@gmail.com>
2023-12-24 13:45:09 -0800

    First commit

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import { TestComponent } from "./components/test";
+
+const Home = () => {
+  return (
+    <main>
+      <div>This is the top</div>
+
+      <div>This is the image fixed</div>
+
+      <div>
+        This is the body
+        <TestComponent />
+      </div>
+    </main>
+  );
+};
+
+export default Home;
